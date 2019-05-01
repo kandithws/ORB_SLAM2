7722c0ab95fad608fbfdbd6e8fedca7711cf6ca4
Kandith Wongsuwan <ws.kandith@gmail.com>
2019-05-01 16:28:02 +0700

    add confidence to LocalBA infomat

--- src/ObjectOptimizer.cpp
+++ src/ObjectOptimizer.cpp
@@ -577,16 +577,9 @@ void Optimizer::LocalBundleAdjustmentWithObjects(KeyFrame *pKF, bool* pbStopFlag
             auto* edgeSE3CuboidProj = new g2o::EdgeSE3CuboidProj(K);
             edgeSE3CuboidProj->setVertex(0, optimizer.vertex(pKFi->mnId));
             edgeSE3CuboidProj->setVertex(1, optimizer.vertex(maxKFId + 1 + pMO->mnId));
-            // TODO
-            edgeSE3CuboidProj->setMeasurement(
-                    Converter::toVector4d(vObservations[vObservationsMap[pMO]]->box()));
-
-
-            //Eigen::Vector4d inv_sigma;
-            //inv_sigma << 1, 1, 1, 1, 1, 1, 1, 1, 1;
-            //inv_sigma = inv_sigma * 2.0;
-            //Eigen::Matrix4d info = inv_sigma.cwiseProduct(inv_sigma).asDiagonal();
-            Eigen::Matrix4d info = Eigen::Matrix4d::Identity() * 2.0;
+            auto obs = vObservations[vObservationsMap[pMO]];
+            edgeSE3CuboidProj->setMeasurement(Converter::toVector4d(obs->box()));
+            Eigen::Matrix4d info = Eigen::Matrix4d::Identity() * obs->_confidence;
             edgeSE3CuboidProj->setInformation(info);
             optimizer.addEdge(edgeSE3CuboidProj);
         }
@@ -606,16 +599,9 @@ void Optimizer::LocalBundleAdjustmentWithObjects(KeyFrame *pKF, bool* pbStopFlag
             auto* edgeSE3CuboidProj = new g2o::EdgeSE3CuboidProj(K);
             edgeSE3CuboidProj->setVertex(0, optimizer.vertex(pKFi->mnId));
             edgeSE3CuboidProj->setVertex(1, optimizer.vertex(maxKFId + 1 + pMO->mnId));
-            // TODO
-            edgeSE3CuboidProj->setMeasurement(
-                    Converter::toVector4d(vObservations[vObservationsMap[pMO]]->box()));
-
-
-            //Eigen::Vector4d inv_sigma;
-            //inv_sigma << 1, 1, 1, 1, 1, 1, 1, 1, 1;
-            //inv_sigma = inv_sigma * 2.0;
-            //Eigen::Matrix4d info = inv_sigma.cwiseProduct(inv_sigma).asDiagonal();
-            Eigen::Matrix4d info = Eigen::Matrix4d::Identity() * 2.0;
+            auto obs = vObservations[vObservationsMap[pMO]];
+            edgeSE3CuboidProj->setMeasurement(Converter::toVector4d(obs->box()));
+            Eigen::Matrix4d info = Eigen::Matrix4d::Identity() * obs->_confidence;
             edgeSE3CuboidProj->setInformation(info);
             optimizer.addEdge(edgeSE3CuboidProj);
         }
