fd9d22140554d318d83abe32e9fbe0bff2f009c0
Kandith Wongsuwan <ws.kandith@gmail.com>
2019-11-28 17:45:59 +0700

    new cam params for chairs dataset

--- Examples/Stereo/run_example_tum.sh
+++ Examples/Stereo/run_example_tum.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-DATASET=/media/backup/thesis_dataset/dinning_room/ds1
-CONFIG=thesis_datasets/dining_room_tum.yaml
+# DATASET=/media/backup/thesis_dataset/dinning_room/ds1
+# CONFIG=thesis_datasets/dining_room_tum.yaml
+DATASET=/media/backup/thesis_dataset/chair2/ds3
+CONFIG=thesis_datasets/chair.yaml
 ./stereo_tum ../../Vocabulary/ORBvoc.txt ${CONFIG} ${DATASET} ${DATASET}/association.txt
