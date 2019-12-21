fe4f18838b97ef21adbc0320dbc7802514d0587b
Kandith Wongsuwan <ws.kandith@gmail.com>
2019-12-21 16:49:01 +0700

    create tree dataset

--- Examples/Stereo/run_example_tum.sh
+++ Examples/Stereo/run_example_tum.sh
@@ -2,6 +2,12 @@
 
 # DATASET=/media/backup/thesis_dataset/dinning_room/ds1
 # CONFIG=thesis_datasets/dining_room_tum.yaml
+
+# Chair2 Data set
 DATASET=/media/backup/thesis_dataset/chair2/ds3
 CONFIG=thesis_datasets/chair.yaml
+
+# TREE Dataset
+# DATASET=/media/backup/thesis_dataset/tree_datasets/tree2/1
+# CONFIG=thesis_datasets/tree.yaml
 ./stereo_tum ../../Vocabulary/ORBvoc.txt ${CONFIG} ${DATASET} ${DATASET}/association.txt
