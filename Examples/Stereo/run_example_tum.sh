2fcffdc4cc6c6957c08116a6bfe4f326b882ee4b
Kandith Wongsuwan <ws.kandith@gmail.com>
2019-12-24 18:14:08 +0700

    update config for treex-3 dataset

--- Examples/Stereo/run_example_tum.sh
+++ Examples/Stereo/run_example_tum.sh
@@ -4,10 +4,11 @@
 # CONFIG=thesis_datasets/dining_room_tum.yaml
 
 # Chair2 Data set
-DATASET=/media/backup/thesis_dataset/chair2/ds3
-CONFIG=thesis_datasets/chair.yaml
+#DATASET=/media/backup/thesis_dataset/chair2/ds3
+#CONFIG=thesis_datasets/chair.yaml
 
 # TREE Dataset
 # DATASET=/media/backup/thesis_dataset/tree_datasets/tree2/1
-# CONFIG=thesis_datasets/tree.yaml
+DATASET=/home/kandithws/tree_dataset2/tree2-2/1
+CONFIG=thesis_datasets/tree.yaml
 ./stereo_tum ../../Vocabulary/ORBvoc.txt ${CONFIG} ${DATASET} ${DATASET}/association.txt
